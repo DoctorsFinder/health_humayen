86a1acaa7ef8a1c679fcaf980cbca4cba9ca3115
NHSanto <nahid170053@diit.edu.bd>
2022-07-07 15:35:12 +0600

    eslint false

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  "eslint": {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    "ignoreDuringBuilds": true
+  }
 };
